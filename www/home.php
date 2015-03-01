af634e920d0c61dac6b91eb2686c7fce87107c9a
Paul <paullj1@gmail.com>
2015-03-01 15:18:25 -0600

    Consolidated SQL Calls to a single call per file

--- www/home.php
+++ www/home.php
@@ -4,32 +4,32 @@ require_once "connect.php"; // Gives us $con
 require_once "thermo_functions.php";
 require_once "constants.php";
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.CURRENT_SETPOINT_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$qry_str = 'SELECT  `value` FROM `status` WHERE `id` 
+	IN ('.CURRENT_SETPOINT_ID.', '.CURRENT_TEMP_ID.', '.MODE_ID.',
+			'.OCCUPIED_ID.', '.HEAT_STATUS_ID.', '.COOL_STATUS_ID.',
+			'.FAN_STATUS_ID.');';
+
+$result = query_db($con, $qry_str);
+
+$entry = fetch_array_db($result);
 $return['current_setpoint'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.CURRENT_TEMP_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $return['current_temp'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.MODE_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $return['mode'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.OCCUPIED_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $return['occupied'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.HEAT_STATUS_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $return['heat_status'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.COOL_STATUS_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $return['cool_status'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.FAN_STATUS_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $return['fan_status'] = $entry['value'];
 
 echo json_encode($return);
--- www/settings.php
+++ www/settings.php
@@ -4,40 +4,38 @@ require_once "connect.php"; // Gives us $con
 require_once "thermo_functions.php";
 require_once "constants.php";
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.FAN_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$qry_str = 'SELECT  `value` FROM `status` WHERE `id`
+	IN ('.FAN_ID.','.OVERRIDE_ID.','.MODE_ID.','.UNOCCUPIED_HEAT_ID.',
+	'.UNOCCUPIED_COOL_ID.','.DAY_OCCUPIED_HEAT_ID.','.DAY_OCCUPIED_COOL_ID.',
+	'.NIGHT_OCCUPIED_HEAT_ID.','.NIGHT_OCCUPIED_COOL_ID.');';
+
+$result = query_db($con, $qry_str);
+
+$entry = fetch_array_db($result);
 $result['fan'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.OVERRIDE_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['override'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.MODE_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['mode'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.UNOCCUPIED_HEAT_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['unoccupied_heat'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.UNOCCUPIED_COOL_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['unoccupied_cool'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.DAY_OCCUPIED_HEAT_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['day_occupied_heat'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.DAY_OCCUPIED_COOL_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['day_occupied_cool'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.NIGHT_OCCUPIED_HEAT_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['night_occupied_heat'] = $entry['value'];
 
-$qry_str = 'SELECT  `value` FROM `status` WHERE `id`='.NIGHT_OCCUPIED_COOL_ID.';';
-$entry = fetch_array_db(query_db($con, $qry_str));
+$entry = fetch_array_db($result);
 $result['night_occupied_cool'] = $entry['value'];
 
 echo json_encode($result);
