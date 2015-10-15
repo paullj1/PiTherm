d1b82959133c7cd486eee059fea08d928e5d6834
Paul <paullj1@gmail.com>
2015-10-15 22:15:02 +0000

    Adding comments and 1wire install

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # Install PiTherm
 
-#echo 'airmon-ng start wlan0' >> /etc/rc.local
+# Setting up 1wire sensor
+modprobe w1-gpio
+modprobe w1-therm
 
+# Configuring wlan0 to run in monitor mode on boot
+echo 'airmon-ng start wlan0' >> /etc/rc.local
+
+# Installing radar
 cp radar.py /usr/local/bin/radar
 chmod +x    /usr/local/bin/radar
 
+# Installing daemon
 cp thermo.py /usr/local/bin/pitherm
 chmod +x     /usr/local/bin/pitherm
 
@@ -16,4 +23,5 @@ chmod +x   /etc/init.d/pitherm
 
 update-rc.d pitherm defaults
 
+# Installing web front-end
 cp -ur ./www/* /var/www/
