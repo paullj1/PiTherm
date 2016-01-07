c237bac519399ade232fa7857aea3a386b0349cc
Paul <paullj1@gmail.com>
2016-01-07 10:02:09 -0500

    adding log message when service crashes

--- extras/watchdog.sh
+++ extras/watchdog.sh
@@ -3,7 +3,8 @@ service=pitherm
 
 if (( $(ps -ef | grep -v grep | grep -v pitherm.local | grep pitherm | wc -l) > 0 ))
 then
-echo "$service is running!!!"
+echo "$service is running!!!" > /dev/null
 else
 /etc/init.d/$service restart
+echo "$service crashed at `date`, restarting..." >> /var/log/pitherm.log
 fi
